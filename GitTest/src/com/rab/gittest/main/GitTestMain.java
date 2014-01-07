553732f1c19e9253f57b02d65faea5cf755e20da
rabautista <rafael.bautista@pcmall.com>
2014-01-07 15:33:41 +0800

    Test

--- GitTest/src/com/rab/gittest/main/GitTestMain.java
+++ GitTest/src/com/rab/gittest/main/GitTestMain.java
@@ -4,8 +4,8 @@ public class GitTestMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hi!");
-		System.out.println("this is a change");
-		System.out.println("Hi!");
+		//System.out.println("this is a change");
+		///System.out.println("Hi!");
 		System.out.println("this is a change");
 	}
 
